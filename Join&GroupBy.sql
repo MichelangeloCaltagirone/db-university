43a4864be8918cd8f55397be4203a3100a02f64e
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-10-17 18:43:32 +0200

    Mancano ancora delle query, esercizio non finito

--- Join&GroupBy.sql
+++ Join&GroupBy.sql
@@ -1,3 +1,7 @@
+
+-- Group By query
+
+
 -- Contare quanti iscritti ci sono stati ogni anno
 SELECT COUNT(*) AS `Numero_Iscritti`, YEAR(`enrolment_date`) AS `Annata` FROM `students` GROUP BY `Annata`;
 
@@ -9,3 +13,25 @@ SELECT AVG(`vote`) AS `Media_Voti`, `exam_id` AS `Esame` FROM `exam_student` GRO
 
 -- Contare quanti corsi di laurea ci sono per ogni dipartimento
 SELECT COUNT(*) AS `Numero_di_Corsi`, `department_id` AS `Per_Dipartimento` FROM `degrees` GROUP BY `Per_Dipartimento`;
+
+
+-- Join query
+
+-- Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+SELECT `degrees`.`name`, `students`.`id` 
+FROM `students` INNER JOIN `degrees` 
+ON `students`.`degree_id` = `degrees`.`id` 
+WHERE `degrees`.`name` = "Corso di Laurea in Economia";
+
+-- Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
+SELECT `degrees`.`name` AS `Corsi_Magistrali_Dipartimento_Neuroscenze`
+FROM `departments` INNER JOIN `degrees`
+ON `departments`.`id` = `degrees`.`department_id`
+WHERE `degrees`.`level` = "Magistrale" AND `departments`.`name` = "Dipartimento di
+ Neuroscienze";   -- NON FUNZIONA !!!
+
+-- Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT `courses`.`name` 
+FROM `courses` INNER JOIN `course_teacher`
+ON `courses`.`id` = `course_teacher`.`course_id`
+WHERE `course_teacher`.`teacher_id` = 44;
